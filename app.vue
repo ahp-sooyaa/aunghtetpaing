8dd22ea6b272e901a841f74bcde7d096e8870f17
Aung Htet Paing <sooyaa894@gmail.com>
2023-01-11 15:36:08 +0630

    WIP

--- app.vue
+++ app.vue
@@ -1,12 +1,94 @@
 <template>
-    <div class="min-h-screen grid place-content-center">
-        <article class="prose prose-stone">
-            <h1>Aung Htet Paing</h1>
-            <p>
-                Hey, I'm Aung Htet Paing, a electronic major student who is
-                interesting in web development. I enjoy watching variety shows,
-                drama, movies and screen cast from Laracasts.
-            </p>
-        </article>
+    <div class="bg-[#010101]">
+        <div class="relative min-h-screen grid place-content-center px-4">
+            <div class="bg-[#EC921F] w-24 h-24 blur-3xl absolute top-0 left-0"></div>
+            <button class="fixed top-5 right-14 bg-gray-800 p-3 rounded-md">
+                <img src="moon.svg" alt="moon svg" />
+            </button>
+
+            <div class="flex flex-col-reverse lg:flex-row items-center">
+                <article class="prose prose-invert">
+                    <h1>Aung Htet Paing</h1>
+                    <p>
+                        Hey, I'm a electronic major student who is
+                        interesting in web development. I enjoy watching variety shows,
+                        drama, movies and screen cast from Laracasts.
+                    </p>
+                    <div class="not-prose flex space-x-5">
+                        <img src="twitter.svg" alt="twitter svg" width="20" height="20" />
+                        <img src="linkedin.svg" alt="linkedin svg" width="20" height="20" />
+                        <img src="github.svg" alt="github svg" width="20" height="20" />
+                    </div>
+                </article>
+                <img src="river-fx-MqsqeQ54yFo-unsplash.png" alt="light bulb">
+            </div>
+            <div class="mt-10">
+                <div class="text-sm text-gray-500">Tech stack I'm familiar with</div>
+                <div class="flex space-x-5 mt-3">
+                    <img src="html.svg" alt="twitter svg" width="20" height="20" />
+                    <img src="css.svg" alt="twitter svg" width="20" height="20" />
+                    <img src="laravel.svg" alt="twitter svg" width="20" height="20" />
+                    <img src="vuejs.svg" alt="twitter svg" width="20" height="20" />
+                    <img src="html.svg" alt="twitter svg" width="20" height="20" />
+                    <img src="css.svg" alt="twitter svg" width="20" height="20" />
+                </div>
+            </div>
+        </div>
+
+        <section class="relative prose prose-invert max-w-5xl mx-auto mb-52 px-4 lg:px-8">
+            <div class="bg-[#EC921F] w-32 h-32 blur-[100px] absolute top-0 right-0"></div>
+            <div class="bg-[#FF6C00] w-20 h-20 blur-[100px] absolute top-40 right-40"></div>
+            <h1 class="text-center">Experience</h1>
+            <div class="pl-5 relative">
+                <div class="bg-gradient-to-b from-transparent via-[#FF6C00] to-transparent h-full w-[1px] absolute left-0"/>
+                <div class="not-prose text-sm text-gray-500">
+                    Jan 2022 - present
+                </div>
+                <div>
+                    <h2 class="mt-0 mb-3">Junior Web Developer @ Myintthukanandi</h2>
+                    <p class="max-w-3xl">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis beatae itaque praesentium nesciunt est ullam quia rerum suscipit. Similique, tempora!
+                    </p>
+                </div>
+            </div>
+            <div class="relative pl-5">
+                <div class="bg-gradient-to-b from-transparent via-[#FF6C00] to-transparent h-full w-[1px] absolute left-0"/>
+                <div class="not-prose text-sm text-gray-500">
+                    Jan 2022 - present
+                </div>
+                <div>
+                    <h2 class="mt-0 mb-3">Junior Web Developer @ Myintthukanandi</h2>
+                    <p class="max-w-3xl">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis beatae itaque praesentium nesciunt est ullam quia rerum suscipit. Similique, tempora!
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="prose prose-invert max-w-5xl mx-auto px-4 lg:px-8">
+            <h1 class="text-center">Projects</h1>
+            <div class="grid lg:grid-cols-2 gap-5">
+                <div class="border-2 border-gray-900 hover:border-transparent bg-gradient-to-r from-[#EC921F] to-[#FF6C00] rounded-2xl">
+                    <div class="bg-black hover:bg-[#0f0f0f] p-5 rounded-[0.9rem]">
+                        <!-- <div class="aspect-[2/1] relative mt-0 rounded-lg h-52 rounded-md mb-5 shadow-lg overflow-hidden">
+                            <img src="koob.png" alt="koob" class="absolute inset-0 h-full">
+                        </div> -->
+                        <div class="mt-0 rounded-lg h-52 rounded-md mb-5 shadow-lg bg-[url('koob.png')] bg-cover bg-center" />
+                        <div class="text-sm text-gray-500">
+                            ecommerce site
+                        </div>
+                        <a href="https://koobdemo.site" class="m-0 no-underline hover:underline">
+                            <span class="absolute inset-0 rounded-2xl"></span>
+                            Koob
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="relative bg-[url('scattered-forcefields.svg')] h-72 text-center flex items-end justify-center">
+            <div class="absolute inset-0 bg-gradient-to-b from-black to-transparent" />
+            <p class="mb-10 text-gray-200">&copy; 2023 AungHtetPaing. All rights reserved.</p>
+        </footer>
     </div>
 </template>
